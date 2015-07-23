809818beb98d82f07e06b9c60bcc978560027bcf
Dipti <dipti@intelligrape.com>
2015-07-23 16:56:20 +0530

    23rd july

--- bundle/src/main/java/com/mycompany/myproject/package-info.java
+++ bundle/src/main/java/com/mycompany/myproject/package-info.java
@@ -3,5 +3,4 @@ package com.mycompany.myproject;
 
 import aQute.bnd.annotation.Version;
 
-        system.out.println("Today is 21st July 2015") ;
-        system.out.println("Today is 21st July 2015 and time is 3:40PM") ;
+        system.out.println("Today is 23rd July 2015") ;
